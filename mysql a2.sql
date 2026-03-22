7d55951b4309e691715bc5f99fe475612cc1f450
sriharisp15 <srihari.sp15@gmail.com>
2026-03-23 00:07:06 +0530

    Delete mysql a2.sql

--- mysql a2.sql
+++
@@ -1,6 +0,0 @@
-alter table officers add column officers_id int;
-SELECT officer_id, department, dept_id FROM officers;
-SELECT officers_id, department, dept_id FROM officers;
-ALTER TABLE officers
-ADD CONSTRAINT fk_dept
-FOREIGN KEY (dept_id) REFERENCES departments(dept_id);
